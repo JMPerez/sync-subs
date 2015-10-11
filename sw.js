3e288327fe6ea3a506a2126214a9f7bac0a4e3aa
José M. Pérez <jperez@spotify.com>
2015-10-11 10:23:59 +0200

    Update GH pages

--- sw.js
+++ sw.js
@@ -3,13 +3,10 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('sync-subs-static-v1').then(function(cache) {
-      var parts = location.pathname.split('/');
-      parts.pop();
-      var path = parts.join('/');
       return cache.addAll([
-        path + '/',
-        path + '/css/style.css',
-        path + '/js/app.js'
+        '/',
+        '/css/style.css',
+        '/js/app.js'
       ]);
     }).catch(function(error) {
       console.error(error);
